c92fecac7cb9bd81646a5abdc2c6f58825e6dc1d
wolfwald <malzig.lounges_0r@icloud.com>
2024-07-18 10:44:02 +0200

    Update ampel_checker.py
    zurück zu rot

--- ampel_checker.py
+++ ampel_checker.py
@@ -37,5 +37,5 @@ def send_email_notification():
         server.sendmail(email_user, to_email, msg.as_string())
 
 if __name__ == "__main__":
-    if get_current_light(soup) != 'grün':
+    if get_current_light(soup) != 'red':
         send_email_notification()
