532d71020fc5b950a7a5ab63d992b9192776c6fe
wolfwald <malzig.lounges_0r@icloud.com>
2024-07-18 10:36:21 +0200

    Update ampel_checker.py
    Logik geändert zu !=

--- ampel_checker.py
+++ ampel_checker.py
@@ -37,5 +37,5 @@ def send_email_notification():
         server.sendmail(email_user, to_email, msg.as_string())
 
 if __name__ == "__main__":
-    if get_current_light(soup) == 'grün':
+    if get_current_light(soup) != 'grün':
         send_email_notification()
