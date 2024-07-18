078f95211a255ea58f5753a226d9958cf81d9a26
wolfwald <malzig.lounges_0r@icloud.com>
2024-07-18 10:44:39 +0200

    Update ampel_checker.py
    en -> de

--- ampel_checker.py
+++ ampel_checker.py
@@ -37,5 +37,5 @@ def send_email_notification():
         server.sendmail(email_user, to_email, msg.as_string())
 
 if __name__ == "__main__":
-    if get_current_light(soup) != 'red':
+    if get_current_light(soup) != 'rot':
         send_email_notification()
